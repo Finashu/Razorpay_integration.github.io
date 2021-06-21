9f00795d6277e9187b9c269bfb44f45909f3d49f
Finashu <63461224+Finashu@users.noreply.github.com>
2021-06-21 15:58:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -17,6 +17,24 @@ app.get('/',(req, res) => {
     res.render('razorpay.ejs')
 })
 
+app.use( express.static( "public" ) );
+
+
+app.get('/', function(req, res) {
+    var mascots = [
+      { name: 'Sammy', organization: "DigitalOcean", birth_year: 2012},
+      { name: 'Tux', organization: "Linux", birth_year: 1996},
+      { name: 'Moby Dock', organization: "Docker", birth_year: 2013}
+    ];
+    var tagline = "No programming concept is complete without a cute animal mascot.";
+  
+    res.render('pages/index', {
+      mascots: mascots,
+      tagline: tagline
+    });
+  });
+
+
 app.post('/orders', (req, res) => {
     let options = {
         amount: 100000,  // amount in the smallest currency unit
@@ -40,5 +58,10 @@ app.post( '/is-order-completed', (req, res) => {
     })    
 
 })
+
+
+app.get('/about', function(req, res) {
+    res.render('pages/about');
+  });
 
 app.listen(1000)
